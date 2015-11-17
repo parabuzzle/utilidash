f11d68a84b298b479555b36e60c6933a7c5bdfc3
Mike Heijmans <parabuzzle@gmail.com>
2015-11-16 16:33:20 -0800

    tune the graph

--- public/js/current_power_creation.js
+++ public/js/current_power_creation.js
@@ -1,11 +1,18 @@
 $(function(){
 
+var wattsLast = 1
+var wattsTemp = 0
+var wattsCurrent = 0
 
 var chart = $('#currentWattsChart')
    .epoch({
-        type: 'time.area',
+        type: 'time.line',
         data: [ {label: "Current Watt Production", values: [{time: (new Date().getTime() / 1000), y:0}]} ],
-        axes: ['left', 'bottom', 'right']
+        axes: ['left', 'bottom', 'right'],
+        ticks: { time: 30, right: 10, left: 10 },
+        windowSize: 500,
+        historySize: 500,
+        range: [0, 5000]
     });
 
     window.setInterval(function() {
@@ -15,7 +22,15 @@ var chart = $('#currentWattsChart')
                 y: d.wattsNow
             }];
             chart.push(dataToPush);
-            $('#watts').text('Current Watts being produced: ' + d.wattsNow)
+
+            wattsCurrent = d.wattsNow
+            if (wattsTemp != wattsCurrent) {
+                wattsLast = wattsTemp
+                wattsTemp = wattsCurrent
+            }
+
+            $('#wattsCurrent').text('Current Watts being produced: ' + wattsCurrent)
+            $('#wattsLast').text('Previous: ' + wattsLast)
         }, "json" );
     }, 1000);
 
